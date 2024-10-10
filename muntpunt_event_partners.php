36ff745a618e42aac4c4d43e6d200592c619731a
Alain Benbassat <alain@businessandcode.eu>
2024-10-10 19:24:28 +0200

    civix upgrade

--- muntpunt_event_partners.php
+++ muntpunt_event_partners.php
@@ -24,59 +24,12 @@ function muntpunt_event_partners_civicrm_install(): void {
 }
 
 /**
- * Implements hook_civicrm_postInstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_postInstall
- */
-function muntpunt_event_partners_civicrm_postInstall(): void {
-  _muntpunt_event_partners_civix_civicrm_postInstall();
-}
-
-/**
- * Implements hook_civicrm_uninstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_uninstall
- */
-function muntpunt_event_partners_civicrm_uninstall(): void {
-  _muntpunt_event_partners_civix_civicrm_uninstall();
-}
-
-/**
  * Implements hook_civicrm_enable().
  *
  * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_enable
  */
 function muntpunt_event_partners_civicrm_enable(): void {
   _muntpunt_event_partners_civix_civicrm_enable();
-}
-
-/**
- * Implements hook_civicrm_disable().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_disable
- */
-function muntpunt_event_partners_civicrm_disable(): void {
-  _muntpunt_event_partners_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_upgrade
- */
-function muntpunt_event_partners_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _muntpunt_event_partners_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implements hook_civicrm_entityTypes().
- *
- * Declare entity types provided by this module.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
- */
-function muntpunt_event_partners_civicrm_entityTypes(&$entityTypes): void {
-  _muntpunt_event_partners_civix_civicrm_entityTypes($entityTypes);
 }
 
 // --- Functions below this ship commented out. Uncomment as required. ---
